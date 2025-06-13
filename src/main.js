939c87115d8450ed5a4bb9cdb59ffb8f7814a238
Liam Pustam <liampustam@gmail.com>
2025-06-13 13:35:15 -0400

    added buttons

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
+let points = 0
 
+let pointCounter = document.getElementById("pointCounter")
+
+function changePoint(amount) {
+
+  points = points + amount
+
+  console.log(points)
+  
+  pointCounter.innerText = points.toFixed(2) + ' Points'
+}
+
+function addPoint() {
+  points = points + 1 
+  console.log(points)
+  document.getElementsById("counter").innerText - "ANIMALS" + points ;
+}
+
+
+function bigClick() {
+  let basePoint = 1
+
+
+
+  changePoint(basePoint)
+}
